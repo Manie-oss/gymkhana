979e32af40ca86ec47d6b18d1ac9dec9b7360de7
Mansi Patel <to.mansip@gmail.com>
2022-07-04 15:25:45 +0530

    added sidebar

--- ui/src/Routes.tsx
+++ ui/src/Routes.tsx
@@ -1,13 +1,32 @@
-import {Route, Routes} from 'react-router-dom';
+import React from 'react';
+import {Route, Routes, useLocation} from 'react-router-dom';
+// import {ChromeLogo} from './assets/images/googlechrome-logo';
+// import {WallclockLogo} from './assets/images/wallclock-logo';
+import {Sidebar} from './components/Sidebar/Sidebar';
 import {Home} from './pages/Home';
 import {Login} from './pages/Login';
 
 export const Router: React.FC = () => {
+  /*  const location = useLocation();
+  const sidebarData = [
+    {
+      name: 'Browse',
+      icon: ChromeLogo,
+      isActive: location.pathname.startsWith('/sidebar'),
+    },
+    {
+      name: 'Recent',
+      icon: WallclockLogo,
+      isActive: location.pathname.startsWith('/sidebar'),
+    },
+  ]; */
   return (
     <>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
+        {/* <Route path="/sidebar" element={<Sidebar rows={sidebarData} />} /> */}
+        <Route path="/sidebar" element={<Sidebar />} />
       </Routes>
     </>
   );
--- ui/src/assets/index.ts
+++ ui/src/assets/index.ts
@@ -2,3 +2,34 @@
 export const logo = new URL('./images/logo.svg', import.meta.url).href;
 export const loginArrow = new URL('./images/login-arrow.svg', import.meta.url)
   .href;
+export const googleChromeLogo = new URL(
+  './images/googleChromeLogo.svg',
+  import.meta.url
+).href;
+export const wallClockLogo = new URL(
+  './images/wallClockLogo.svg',
+  import.meta.url
+).href;
+export const calenderLogo = new URL(
+  './images/calenderLogo.svg',
+  import.meta.url
+).href;
+
+export const desktopTower = new URL(
+  './images/desktopTower.svg',
+  import.meta.url
+).href;
+
+export const theatreLogo = new URL('./images/theatreLogo.svg', import.meta.url)
+  .href;
+
+export const sportsLogo = new URL('./images/sportsLogo.svg', import.meta.url)
+  .href;
+
+export const settingsLogo = new URL(
+  './images/settingsLogo.svg',
+  import.meta.url
+).href;
+
+export const signoutLogo = new URL('./images/signoutLogo.svg', import.meta.url)
+  .href;
